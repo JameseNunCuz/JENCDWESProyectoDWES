e908730e575d7482bb6aa8476e84fa5476c3ad48
JameseNunCuz <jamese.nuncuz.1@educa.jcyl.es>
2025-10-24 10:32:02 +0200

    Initail Commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James Edward Nuñez Cuzcano</title>
+    <link rel="stylesheet" href="./webroot/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Desarrollo web en entorno servidor</h1>
+        </div>
+        <button class="home active" onclick="location.href = '../'" ><img src="./webresources/home.png"></button>
+    </div>
+
+    <div class="center-container">
+        <button onclick="window.open('./doc/EjerciciosTema1.pdf')" class="active">
+            <p>Tema 1: Desarollo web en entorno servido</p>
+        </button>
+        <button onclick="location.href = '../JENCDAWProyectoDAW/README.html'" class="active">
+            <p>Tema 2: Instalación, configuración y documentación del entorno de desarrollo y del entorno de explotación</p>
+        </button>
+        <button onclick="location.href = '../JENCDWESProyectoTema3'" class="active">
+            <p>Tema 3: Caracteristicas del lenguaje PHP</p>
+        </button>
+        <button>
+            <p>Tema 4: Técnicas de acceso a datos en PHP</p>
+        </button>
+        <button>
+            <p>Tema 5: Desarrollo de aplicaciones web</p>
+        </button>
+    </div>
+
+    <div class="footer">
+        <h3>James Edward Nuñez Cuzcano</h3>
+    </div>
+</body>
+
+</html>
